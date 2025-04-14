4d3dbd923f7f9bd3aecca3dd52b1653b947899e8
Petrit-Halabaku <phalabaku@gmail.com>
2025-04-15 00:04:24 +0200

    Update dependencies and modify routes to use Maintenance page

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,15 +1,13 @@
 // import React from "react";
 import { RouteObject } from "react-router-dom";
-import { Home } from "../pages/Home";
-import { Terms } from "../pages/Terms";
-import { Privacy } from "../pages/Privacy";
-import { NotFound } from "../pages/NotFound";
+import Maintenance from "../pages/Maintenance";
 // Import other pages as needed
 
 export const routes: RouteObject[] = [
-  { path: "/", element: <Home /> },
-  { path: "/terms", element: <Terms /> },
-  { path: "/privacy", element: <Privacy /> },
-  { path: "*", element: <NotFound /> },
+  { path: "/", element: <Maintenance /> },
+  { path: "/terms", element: <Maintenance /> },
+  { path: "/privacy", element: <Maintenance /> },
+  { path: "/maintenance", element: <Maintenance /> },
+  { path: "*", element: <Maintenance /> },
   // Add more routes as needed
 ];
