f602a60d17ee22726a85a40028262d48500b4e33
Petrit-Halabaku <phalabaku@gmail.com>
2025-05-02 00:25:08 +0200

    Update routes to use Home, Terms, Privacy, and NotFound components; upgrade caniuse-lite dependency in package-lock.json

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,18 @@
 // import React from "react";
 import { RouteObject } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Terms } from "../pages/Terms";
+import { Privacy } from "../pages/Privacy";
+import { NotFound } from "../pages/NotFound";
+
 import Maintenance from "../pages/Maintenance";
 // Import other pages as needed
 
 export const routes: RouteObject[] = [
-  { path: "/", element: <Maintenance /> },
-  { path: "/terms", element: <Maintenance /> },
-  { path: "/privacy", element: <Maintenance /> },
+  { path: "/", element: <Home /> },
+  { path: "/terms", element: <Terms /> },
+  { path: "/privacy", element: <Privacy /> },
   { path: "/maintenance", element: <Maintenance /> },
-  { path: "*", element: <Maintenance /> },
+  { path: "*", element: <NotFound /> },
   // Add more routes as needed
 ];
